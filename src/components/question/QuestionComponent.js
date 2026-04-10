d7c035aff797fa66f1ab148391ec2900ed2031be
23wh1a0532 <23wh1a0532@bvrithyderabad.edu.in>
2026-04-10 19:57:13 +0530

    changed the QuestionComponent

--- src/components/question/QuestionComponent.js
+++ src/components/question/QuestionComponent.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import react,{useState,useEffect} from "react-router-dom";
+
+import {useState,useEffect} from "react";
 
 // TODO: Import any API functions you need from '../../api/client'
 // Example: import { get, post } from '../../api/client';
 
 function QuestionComponent() {
   // TODO: Define state variables needed for your question set
-  
+  const [tasks,setTasks] = useState([]);
+  useEffect(()=>{
+    fetch("http://bvrithcloud.com/api/tasks",{
+      headers:{
+        "x-student-id":"23WH1A0532"
+      }
+
+    })
+    .then((res) => res.json())
+    .then((data)=>setTasks(data))
+    .catch((err)=>console.log(err));
+  },[]);
+  return(
+    <div>
+      <h2>Task List</h2>
+      {tasks.map((task)=>(
+        <div key={task._id}>
+          <h4>{task.title}</h4>
+          </div>
+      ))}
+    </div>
+  );
+}
 
   
 
@@ -19,15 +41,6 @@ function QuestionComponent() {
   // TODO: Implement any event handlers required by your question set
   
 
-  return (
-    <div>
-      {/* TODO: Replace this placeholder with your question set UI */}
-      
-      <p>QuestionComponent placeholder — implement your assigned question set here.</p>
-
-      {/* TODO: Render fetched data or form elements as required */}
-    </div>
-  );
-}
+  
 
 export default QuestionComponent;
